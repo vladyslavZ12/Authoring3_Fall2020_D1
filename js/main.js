da2439971cef6f472ca4896b3e029453cb5d01c5
tvanrys <tvanrys@fanshaweonline.ca>
2020-09-24 16:07:14 -0400

    finished build files

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// import your packages here
+import Person from "./modules/DataModule.js";
+import NavSystem from "./modules/TheNavSystem.js";
+
+(() => {
+    // stub * just a place for non-component-specific stuff
+    console.log('loaded');
+
+    console.log(Person);
+})();
