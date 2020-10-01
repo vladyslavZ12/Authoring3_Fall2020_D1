0ef9f658f10f6662c4aa723b273500c8c62d628a
tvanrys <tvanrys@fanshaweonline.ca>
2020-10-01 13:21:38 -0400

    adding SASS

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 // import your packages here
 import Person from "./modules/DataModule.js";
-import NavSystem from "./modules/TheNavSystem.js";
 
 (() => {
     // stub * just a place for non-component-specific stuff
--- js/modules/DataModule.js
+++ js/modules/DataModule.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
     name: "Trevor",
     role: "Prof",
     nickname: "Tervor"
