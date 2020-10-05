40cf2dcf08b7d6ce89f2ad179dfd8ccd2c381ec0
unknown <tvanrys@fanshaweonline.ca>
2020-10-05 13:10:01 -0400

    addin async

--- js/main.js
+++ js/main.js
@@ -4,65 +4,52 @@ import Team from "./modules/DataModule.js";
 (() => {
     // stub * just a place for non-component-specific stuff
     console.log('loaded');
-
-    //console.log(Team);
-
-    //debugger;
-
-    // select our user elements and load the content
-    // let userSection = document.querySelector(".user-section").children;
-
-    // userSection[1].textContent = Team["Justin"].name;
-    // userSection[2].textContent = Team["Justin"].role;
-    // userSection[3].textContent = Team["Justin"].nickname;
-
-    // XHTTP is the old tried-and-true way of doing AJAX - still relevant, but there are better options
-    let myReq = new XMLHttpRequest;
-    myReq.addEventListener('readystatechange', handleRequest);
-
-    myReq.open('GET', '../DataSet.json');
-    myReq.send();
-
-    function handleRequest() {
-        if (myReq.readyState === XMLHttpRequest.DONE) {
-            // check status here and proceed
-            if (myReq.status === 200) {
-                // 200 means done and dusted, ready to go with the dataset!
-                handleDataSet(myReq.responseText);
-
-            } else {
-                // probably got some kind of error code, so handle that 
-                // a 404, 500 etc... can render appropriate error messages here
-                console.error(`${myReq.status} : something done broke, son`);
-            }
-        } else {
-            // request isn't ready yet, keep waiting...
-            console.log(`Request state: ${myReq.readyState}. Still processing...`);
-        }
-
+    
+    function popErrorBox(message) {
+        alert("Something has gone horribly, horribly wrong");
     }
 
     function handleDataSet(data) {
-        let myData = JSON.parse(data),
-            userSection = document.querySelector('.user-section'),
+        let userSection = document.querySelector('.user-section'),
             userTemplate = document.querySelector('#user-template').content;
 
-        debugger;
-
-        for (let user in myData) {
+        for (let user in data) {
             let currentUser = userTemplate.cloneNode(true),
                 currentUserText = currentUser.querySelector('.user').children;
 
-            currentUserText[1].textContent = myData[user].name;
-            currentUserText[2].textContent = myData[user].role;
-            currentUserText[3].textContent = myData[user].nickname;
+            currentUserText[1].textContent = data[user].name;
+            currentUserText[2].textContent = data[user].role;
+            currentUserText[3].textContent = data[user].nickname;
 
             // add this new user to the view
             userSection.appendChild(currentUser);
         }
-
-        console.log(data);
     }
+
+    // async is now THE WAY to do AJAX / asynchronous operations
+    async function fetchData() {
+        // ask for a resource, and then do something with it when it resolves
+        let resource = await fetch('../DataSt.json').then(response => {
+            if (response.status !== 200) {
+                console.error('server done broke!' + response.status);
+                popErrorBox(response);
+
+                return false;
+                // response = JSON.stringify({message: 'some stupid server error'});
+            }
+
+            return response;
+        });
+
+        // fetch uses the Promise API, so it'll return with the resource or return false - either way, it resolves the promise
+
+        // we'll assume success and pass through a parsed JavaScript object from the JSON data we get
+        let dataset = await resource.json();
+
+        return dataset;            
+    }
+
+    fetchData().then(data => handleDataSet(data));
 
     // fetch is an easier syntax to remember, and is pretty well supported but still considered a "base model" way of doing AJAX
 
