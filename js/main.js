a6796a2d0f9d6078a99cd062a36284b907cb26e2
unknown <tvanrys@fanshaweonline.ca>
2020-10-08 16:46:57 -0400

    finished AJAX example

--- js/main.js
+++ js/main.js
@@ -3,6 +3,39 @@ import Team from "./modules/DataModule.js";
 
 (() => {
     // stub * just a place for non-component-specific stuff
+
+    // set up the XMLHttp object
+    let myReq = new XMLHttpRequest;
+    // make sure we can handle whatever data comes back, or any errors
+    myReq.addEventListener("readystatechange", handleRequest);
+
+    // open a request and pass thru the URL of the data that we want
+    myReq.open('GET', '../DataSet.json');
+
+    // actually make the request
+    myReq.send();
+
+    // handleRequest function goes here
+    function handleRequest() {
+        if (myReq.readyState === XMLHttpRequest.DONE) {
+            // check status here and proceed
+            if (myReq.status === 200) {
+                // 200 means done and dusted, ready to go with the dataset!
+                debugger;
+                handleDataSet(JSON.parse(myReq.responseText));
+
+            } else {
+                // probably got some kind of error code, so handle that 
+                // a 404, 500 etc... can render appropriate error messages here
+                console.error(`${myReq.status} : something done broke, son`);
+            }
+        } else {
+            // request isn't ready yet, keep waiting...
+            console.log(`Request state: ${myReq.readyState}. Still processing...`);
+        }
+
+    }
+
     let userSection = document.querySelector(".user-section"),
         userTemplate = document.querySelector("#profs-template").content;
 
@@ -11,6 +44,9 @@ import Team from "./modules/DataModule.js";
     // select our user elements and load the content
 
     function handleDataSet(data) {
+
+        debugger;
+
         for (let user in data) {
 
             // make a copy of our template here and then
@@ -27,6 +63,6 @@ import Team from "./modules/DataModule.js";
         }           
     }
 
-    handleDataSet(Team);
+    // handleDataSet(Team);
 
 })();
