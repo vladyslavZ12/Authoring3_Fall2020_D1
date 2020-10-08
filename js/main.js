288acfd5089131acfa024779795e64789736c88c
unknown <tvanrys@fanshaweonline.ca>
2020-10-08 14:17:14 -0400

    adding dynamic data to our view

--- js/main.js
+++ js/main.js
@@ -3,14 +3,30 @@ import Team from "./modules/DataModule.js";
 
 (() => {
     // stub * just a place for non-component-specific stuff
-    let userSection = document.querySelector(".user-section");
+    let userSection = document.querySelector(".user-section"),
+        userTemplate = document.querySelector("#profs-template").content;
 
     debugger;
 
     // select our user elements and load the content
-    
 
-    userSection[1].textContent = Team["Justin"].name;
-    userSection[2].textContent = Team["Justin"].role;
-    userSection[3].textContent = Team["Justin"].nickname;
+    function handleDataSet(data) {
+        for (let user in data) {
+
+            // make a copy of our template here and then
+            // populate the children (text elements) with
+            // the static data from the Team object
+            let currentUser = userTemplate.cloneNode(true),
+                currentUserText = currentUser.querySelector('.user').children;
+
+            currentUserText[1].textContent = data[user].name;
+            currentUserText[2].textContent = data[user].role;
+            currentUserText[3].textContent = data[user].nickname;
+
+            userSection.appendChild(currentUser);
+        }           
+    }
+
+    handleDataSet(Team);
+
 })();
